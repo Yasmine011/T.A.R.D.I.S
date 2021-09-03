928356dbd6329c91b2ba75a83800e8b0f0315a26
Yasmine  Tillman <87497186+Yasmine011@users.noreply.github.com>
2021-09-03 00:27:40 -0500

    Add files via upload

---
+++ App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './menu_hw/App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ DivTwo.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+import Div3 from './DivThree';
+
+export default class Div2 extends Component{
+    render(){
+        return(
+            <div>
+                <Div3 name={this.props.name} change={this.props.change}/>
+            </div>
+        )
+    }
+}
